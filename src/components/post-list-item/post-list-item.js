257183be2a1720564a14f36f1b06b7bfe2af8d41
Matrix <o.korsakov@matrixb2b.net>
2021-03-12 15:32:43 +0300

    btn star like

--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -1,24 +1,57 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import './post-list-item.css'
 
-const PostListItem = () => {
-    return (
-        <li className="app-list-item d-flex justify-content-between">
-            <span className="app-list-item-lable">
-                Hello World
-            </span>
-            <div className="d-flex justify-content-center align-items-center">
-                <button type="button" className="btn-star btn-sm">
-                    <i className="fa fa-star"></i>
-                </button>
-                <button type="button" className="btn-trash btn-sm">
-                    <i className="fa fa-trash-o"></i>
-                </button>
-                <i className="fa fa-heart"></i>
-            </div>
-        </li>
-    )
-}
+export default class PostListItem extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         important: false,
+    //         like: false
+    //     };
+    //     this.onImportant = this.onImportant.bind(this);
+    //     this.onLike = this.onLike.bind(this);
+    // }
+    state = {
+        important: false,
+        like: false
+    }
+    onImportant = () => {
+        this.setState(({important}) => ({
+            important: !important
+        }))
+    } // при клтке на кнопку вызывается метод работает с state (состоянием поста) дальще функция принимает аргумент работает с ним и возвращает
+    onLike = () => {
+        this.setState(({like}) => ({
+            like: !like
+        }))
+    }
 
-export default PostListItem;
+    render() {
+        const {label} = this.props;
+        const {important, like} = this.state;
+        let classNames = 'app-list-item d-flex justify-content-between'
+        if (important) {
+            classNames += ' important';
+        }
+        if (like) {
+            classNames += ' like';
+        }
+        return (
+            <div className={classNames}>
+                <span className="app-list-item-label" onClick={this.onLike}>
+                    {label}
+                </span>
+                <div className="d-flex justify-content-center align-items-center">
+                    <button type="button" className="btn-star btn-sm" onClick={this.onImportant}>
+                        <i className="fa fa-star"></i>
+                    </button>
+                    <button type="button" className="btn-trash btn-sm">
+                        <i className="fa fa-trash-o"></i>
+                    </button>
+                    <i className="fa fa-heart"></i>
+                </div>
+            </div>
+        )
+    }
+}
